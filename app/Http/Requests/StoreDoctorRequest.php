986c6ccd441faeacb93176cdf20af50c21f04bbf
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-04-05 19:00:34 +0200

    adding validation request to store doctor

---
+++ app/Http/Requests/StoreDoctorRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=> 'required|min:5',
+            'email'=> 'unique:App\User,email|email|required',
+            'password'=> 'required|min:6',
+            'national_id'=> 'required|unique:App\Doctor,national_id|min:10',
+            'avatar'=> 'image|mimes:jpeg,jpg'
+        ];
+    }
+}
