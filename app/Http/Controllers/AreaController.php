11054f26f60cbf56da8b659587f841f24cf69f8c
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-04-07 04:27:14 +0200

    adding actions to area datatable

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -21,12 +21,11 @@ class AreaController extends Controller
     {
         $areas = Area::query();
         return DataTables()::of($areas)
-        ->addColumn('action', function(Area $area) {
-
-            $button = '<a name="show" id="'.$area->id.'" class="show btn btn-success btn-sm" href="/areas/'.$area->id.'">Show</a>';
-            $button .= '<a name="edit" id="'.$area->id.'" class="edit btn btn-primary btn-sm" href="/areas/'.$area->id.'/edit">Edit</a>';
+        ->addColumn('action', function(Area $area){
+            $button = '<a name="show" id="'.$area->id.'" style="border-radius: 20px;" class="show btn btn-success btn-sm p-0" href="/areas/'.$area->id.'"><i class="fas fa-eye m-2"></i></a>';
+            $button .= '<a name="edit" id="'.$area->id.'" style="border-radius: 20px;" class="edit btn btn-primary btn-sm p-0" href="/areas/'.$area->id.'/edit"><i class="fas fa-edit m-2"></i></a>';
             // $button .= '&nbsp;&nbsp;';
-            $button .= '<button type="button" name="delete" id="'.$area->id.'" class="delete btn btn-danger btn-sm">Delete</button>';
+            $button .= '<button type="button" name="delete" id="'.$area->id.'" style="border-radius: 20px;" class="delete btn btn-danger btn-sm p-0"><i class="fas fa-trash m-2"></i></button>';
             return $button;
             
         })
--- resources/views/areas/index.blade.php
+++ resources/views/areas/index.blade.php
@@ -35,34 +35,84 @@
     <!-- /.row -->
   </section>
   <!-- /.content -->
-
 </div>
+
+<div id="confirmModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h2 class="modal-title">Confirmation</h2>
+      </div>
+      <div class="modal-body">
+        <h4 align="center" style="margin:0;">Are you sure you want to remove this data?</h4>
+      </div>
+      <div class="modal-footer">
+        <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
   $(document).ready(function() {
-        $('#areas').DataTable({
-          processing: true,
-          serverSide: true,
-          responsive: true,
-          ajax: "{{ url('/areas') }}",
-          columns: [{
-              data: 'id',
-              name: 'id'
-            },
-            {
-              data: 'name',
-              name: 'name'
-            },
-            {
-              data: 'address',
-              name: 'address'
-            },
-            {
-              data: 'action',
-              name: 'action'
-            },
-          ]
-        });
+    $('#areas').DataTable({
+      processing: true,
+      serverSide: true,
+      responsive: true,
+      ajax: "{{ url('/areas') }}",
+      columns: [{
+          data: 'id',
+          name: 'id'
+        },
+        {
+          data: 'name',
+          name: 'name'
+        },
+        {
+          data: 'address',
+          name: 'address'
+        },
+        {
+          data: 'action',
+          name: 'action'
+        },
+      ]
+    });
+
+
+    $(document).on('click', '.delete', function() {
+      area_id = $(this).attr('id');
+
+      const token = $('meta[name="csrf-token"]').attr('content');
+      console.log(token);
+      $('#confirmModal').modal('show');
+    });
+
+    $('#ok_button').click(function() {
+      const token = $('meta[name="csrf-token"]').attr('content');
+      $.ajax({
+        url: "/areas/" + area_id,
+        type: "delete",
+        data: {
+          'id': area_id,
+          '_token': token,
+        },
+        beforeSend: function() {
+          $('#ok_button').text('Deleting...');
+        },
+        success: function(data) {
+          setTimeout(function() {
+            $('#confirmModal').modal('hide');
+            $('#ok_button').text('OK');
+            $('#areas').DataTable().ajax.reload();
+          }, 2000);
+        }
       })
+    });
+
+  })
 </script>
 
 @endsection
