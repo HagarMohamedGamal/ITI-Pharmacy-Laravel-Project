8520e26f073c0898d53e61af7305e7c32bce78e9
Mahmoud-65 <mahmoudyoussef120@gmail.com>
2020-04-09 01:06:38 +0200

    customizing ajax in revenue blade to retrieve date from revenue controller for admin

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -7,11 +7,12 @@ use App\Pharmacy;
 use Illuminate\Http\Request;
 use DB;
 use Illuminate\Support\Facades\Auth;
+use Datatables;
 class RevenueController extends Controller
 {
 
     public function index()
-    {  
+    {
         //    $id=2;
     //     $ordersCount = DB::table('orders')->where('pharmacy_id',$id)->count();
     //     $totalIncome = DB::table('orders')->where('pharmacy_id',$id)->sum("price");
@@ -31,4 +32,44 @@ class RevenueController extends Controller
             // 'totalIncome' => $totalIncome,
         ]);
     }
+
+
+
+    function getAllData()
+    {
+      // dd("a");
+     //  $id = Auth::id();
+     //  // $pharmacy=Pharmacy::find($id);
+     // $pharmacy = Pharmacy::select('id', 'national_id','priority');
+    // $pharmacy = DB::table('pharmacies')->select('name', 'national_id')->get();
+     // return DataTables::of($pharmacy)->make(true);
+     // return DataTables()::of($pharmacy)->make(true);
+
+     //**********************************************
+       // $id = Auth::id();
+       $pharmacies = Pharmacy::query();
+       return DataTables()::of($pharmacies)
+             ->editColumn('id', function(Pharmacy $pharmacy) {
+                 return $pharmacy->id ;
+             })
+             ->addColumn('name', function(Pharmacy $pharmacy) {
+                  $id = $pharmacy->id;
+                 return User::find($id)->name;
+             })
+             ->addColumn('totalOrders', function(Pharmacy $pharmacy) {
+                  // $id = Auth::id();
+                  $ordersCount = DB::table('orders')->where('pharmacy_id',$pharmacy->id)->count();
+                 return $ordersCount;
+             })
+             ->addColumn('totalRevenue', function(Pharmacy $pharmacy) {
+                  // $id = Auth::id();
+                  $totalIncome = DB::table('orders')->where('pharmacy_id',$pharmacy->id)->sum("price");
+                 return $totalIncome;
+             })
+             ->toJson();
+
+    }
+
+
+
 }
