b89389b43338219a1f2edcac3024aa226d2ef4df
rolaahmed <rolaahmedabdelmaksoud281997@gmail.com>
2020-04-10 12:47:50 +0200

    modify sql query

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -53,7 +53,9 @@ class StripePaymentController extends Controller
     {
 
         $order_id=$request->order_id;
-        $amount = DB::table("orders") ->select(DB::raw("SUM((medicines.price /100)* medicine_order.quantity) as total_price")) ->leftjoin("medicine_order","medicine_order.order_id","=","orders.id")->leftjoin("medicines","medicine_order.medicine_id","=","medicines.id")->where('orders.id',$request->order_id)->first();
+         $amount = DB::table("medicines") ->select(DB::raw("SUM((medicines.price)* medicine_order.quantity) as total_price"))
+            ->leftjoin("medicine_order","medicine_order.medicine_id","=","medicines.id")->where('medicine_order.order_id',$request->order_id)->first();
+
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
 
         Stripe\Charge::create ([
